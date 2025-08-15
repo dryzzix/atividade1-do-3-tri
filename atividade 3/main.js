9a595919b36dc05517409ea75d51881f4c842c8b
dryzzix <gabriel.santana21@escola.pr.gov.br>
2025-08-15 09:58:45 -0300

    Add files via upload

---
+++ atividade 3/main.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
+const resp3 = document.querySelector("#outResp3");
+
+frm.addEventListener("submit", (e) => {
+   const veiculo = frm.inVeiculo.value;
+   const preco = number(frm.inPreco.value);
+
+   const entrada = preco * 0.50;
+   const parcela = (preco * 0.50) / 12;
+
+   resp2.innerText = `promação: ${veiculo}`;
+   resp2.innerText = `entrada de R$: ${entrada.toFixed(2)}`;
+   resp3.innerText = `+12x de R$: ${parcela.toFixed(2)}`;
+   e.preventDefault
+
+})
